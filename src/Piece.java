b9d7b136add4a62a44e6451c78544c087e8b2680
NicolasVera77 <nicocavs@gmail.com>
2017-04-01 16:09:52 +0200

    jdjd

---
+++ src/Piece.java
@@ -0,0 +1,41 @@
+public abstract Class Piece {
+
+	private boolean vivante;
+	private String nom;
+	private String couleur;
+
+	// private Point position;
+	// private int distance;
+	// private int direction;
+
+	public Piece(String nom, String couleur){
+		this.nom = nom;
+		this.couleur = couleur;
+		this.vivante = true;
+	}
+
+	public String getNom(){
+		return this.nom;
+	}
+
+	public void setNom(String nom){
+		this.nom = nom;
+	}
+
+	public String getCouleur(){
+		return this.couleur;
+	}
+
+	public void setCouleur(String couleur){
+		this.couleur = couleur;
+	}
+
+	public boolean getVivante(){
+		return this.vivante;
+	}
+
+	public void setVivante(boolean vivante){
+		this.vivante = vivante
+	}
+
+}
