edaf96dbd3adf8877197917653ebd49cddf206a3
NicolasVera77 <nicocavs@gmail.com>
2017-04-01 17:32:12 +0200

    plateau initialisation()

--- src/Plateau.java
+++ src/Plateau.java
@@ -15,4 +15,35 @@ public class Plateau
 			}
 		}
 	}
+
+	public initialisation()
+	{
+		for(int i=0; i<COLONNE; i++)
+		{
+			this.echiquier[1][i] = new Pion("noir");
+		}
+
+		this.echiquier[0][0] = new Tour("noir");
+		this.echiquier[0][7] = new Tour("noir");
+		this.echiquier[0][1] = new Cavalier("noir");
+		this.echiquier[0][6] = new Cavalier("noir");
+		this.echiquier[0][2] = new Fou("noir");
+		this.echiquier[0][5] = new Fou("noir");
+		this.echiquier[0][3] = new Dame("noir");
+		this.echiquier[0][4] = new Roi("noir");
+
+		for(int i=0; i<COLONNE; i++)
+		{
+			this.echiquier[LIGNE-2][i] = new Pion("blanc");
+		}
+
+		this.echiquier[7][0] = new Tour("blanc");
+		this.echiquier[7][7] = new Tour("blanc");
+		this.echiquier[7][1] = new Cavalier("blanc");
+		this.echiquier[7][6] = new Cavalier("blanc");
+		this.echiquier[7][2] = new Fou("blanc");
+		this.echiquier[7][5] = new Fou("blanc");
+		this.echiquier[7][3] = new Dame("blanc");
+		this.echiquier[7][4] = new Roi("blanc");
+	}
 }
