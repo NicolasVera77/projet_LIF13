da64a13c4f260b2ec658bd513eeefea08c310c79
NicolasVera77 <nicocavs@gmail.com>
2017-04-01 17:17:19 +0200

    debut plateau

--- src/Plateau.java
+++ src/Plateau.java
@@ -1,4 +1,18 @@
 public class Plateau
 {
-	
+	private Case[][] echiquier;	
+	private static final int LIGNE = 8;
+	private static final int COLONNE = 8;
+
+	public Plateau()
+	{
+		echiquier = Case[LIGNE][COLONNE];
+		for(int i=0; i<LIGNE; i++)
+		{
+			for(int j=0; j<COLONNE; j++)
+			{
+				echiquier[i][j] = new Case();
+			}
+		}
+	}
 }
