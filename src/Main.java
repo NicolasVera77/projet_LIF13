e99e9f9bdd7d27449c61ce91856c75a0fc73cfeb
marcellinmichael <marcellin.michael@gmail.com>
2017-04-07 13:54:40 +0200

    creation du main 2

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+public class Main extends Application
+{
+	public static void main(String[] args)
+	{
+		Application.launch(Main.class, args);
+    }
+
+    @Override
+    public void start(Stage primaryStage)
+    {
+
+    }
+}
