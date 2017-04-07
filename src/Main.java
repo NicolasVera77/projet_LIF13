908344b215e6f807cdf6d3fd27994579c4a695ed
NicolasVera77 <nicocavs@gmail.com>
2017-04-07 16:00:00 +0200

    on continue les bails

--- src/Main.java
+++ src/Main.java
@@ -29,57 +29,25 @@ public class Main extends Application
 		Application.launch(Main.class, args);
 	}
 
-	// affiche la saisie et le résultat
-	Text affichage;
-
 	@Override
-	public void start(Stage primaryStage)
-	{
-		primaryStage.setTitle("Jeu d'échecs");
+	public void start(Stage primaryStage) 
+	{		
+		Group root = new Group();
+        Scene scene = new Scene(root, 800, 600, Color.LIGHTBLUE);
+        GridPane gPane = new GridPane();
 
-		// gestion du placement (permet de palcer le champ Text affichage en haut, et GridPane gPane au centre)
-		BorderPane border = new BorderPane();
+        for (int i=0; i<8; i++)
+        {
+        	for (int j=0; j<8; j++)
+        	{
+        		gPane.add(new Button(), i, j);
+        	}	
+        }
 
-		// permet de placer les diffrents boutons dans une grille
-		GridPane gPane = new GridPane();
+        root.getChildren().add(gPane);
+        primaryStage.setScene(scene);
 
-		int column = 0;
-		int row = 0;
 
-		affichage = new Text("");
-		affichage.setFont(Font.font ("Verdana", 20));
-		affichage.setFill(Color.RED);
-		border.setTop(affichage);
-
-		// création des bouton et placement dans la grille
-		for (String s : new String[]{"7", "8", "9", "/", "4", "5", "6", "*", "1", "2", "3", "+", "0", "(", ")"})
-		{
-			final Text t = new Text(s);
-			t.setWrappingWidth(30);
-			t.setFont(Font.font ("Verdana", 20));
-			t.setTextAlignment(TextAlignment.CENTER);
-
-			gPane.add(t, column++, row);
-
-			if (column > 3)
-			{
-				column = 0;
-				row++;
-			}
-		}
-
-		final Text t = new Text("=");
-		t.setWrappingWidth(30);
-		gPane.add(t, column++, row);
-		t.setTextAlignment(TextAlignment.CENTER);
-
-		gPane.setGridLinesVisible(true);
-
-		border.setCenter(gPane);
-
-		Scene scene = new Scene(border, Color.LIGHTBLUE);
-
-		primaryStage.setScene(scene);
-		primaryStage.show();
+        primaryStage.show();
 	}
 }
