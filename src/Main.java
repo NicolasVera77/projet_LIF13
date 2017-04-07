09880d805e59ce2bab8965fa9ba2702fc9c8ac26
marcellinmichael <marcellin.michael@gmail.com>
2017-04-07 14:10:30 +0200

    première fenetre

--- src/Main.java
+++ src/Main.java
@@ -12,11 +12,34 @@ public class Main extends Application
 	public static void main(String[] args)
 	{
 		Application.launch(Main.class, args);
-    }
+	}
 
-    @Override
-    public void start(Stage primaryStage)
-    {
+	@Override
+	public void start(Stage primaryStage)
+	{
+		primaryStage.setTitle("Hello World");
 
-    }
+		Group root = new Group();
+		Scene scene = new Scene(root, 300, 250, Color.LIGHTGREEN);
+		Button btn = new Button();
+
+		btn.setLayoutX(100);
+		btn.setLayoutY(80);
+		btn.setText("Hello World");
+
+		btn.setOnAction(new EventHandler<ActionEvent>()
+		{
+			public void handle(ActionEvent event)
+			{
+				System.out.println("Hello World");
+			}
+
+		});
+
+		root.getChildren().add(btn);       
+
+		primaryStage.setScene(scene);
+		//primaryStage.setVisible(true);
+		primaryStage.show();
+	}
 }
