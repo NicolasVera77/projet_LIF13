7d15f1ed7406c9e88b56afafdecfdc80e4d5e6d0
marcellinmichael <marcellin.michael@gmail.com>
2017-04-07 17:10:20 +0200

    création de la vue de la grille

--- src/Main.java
+++ src/Main.java
@@ -21,9 +21,15 @@ import javafx.scene.text.Font;
 import javafx.stage.Stage;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextAlignment;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.RowConstraints;
 
 public class Main extends Application
 {
+	public static final int WINDOW_WIDTH = 800;
+	public static final int  WINDOW_HEIGHT = 800;
+
 	public static void main(String[] args)
 	{
 		Application.launch(Main.class, args);
@@ -31,18 +37,35 @@ public class Main extends Application
 
 	@Override
 	public void start(Stage primaryStage) 
-	{		
+	{
 		Group root = new Group();
-        Scene scene = new Scene(root, 800, 600, Color.LIGHTBLUE);
+        Scene scene = new Scene(root, WINDOW_WIDTH, WINDOW_HEIGHT, Color.LIGHTBLUE);
         GridPane gPane = new GridPane();
+
+        //gPane.setPrefSize(1000,1000);
 
         for (int i=0; i<8; i++)
         {
+        	// RowConstraints row = new RowConstraints();
+    		// row.setPercentHeight(50);
+    		// gPane.getRowConstraints().add(row);
         	for (int j=0; j<8; j++)
         	{
-        		gPane.add(new Button(), i, j);
-        	}	
+        		ColumnConstraints column = new ColumnConstraints();
+        		column.setPercentWidth(100);
+        		gPane.getColumnConstraints().add(column);
+
+        		Button b = new Button();
+        		b.setMaxWidth(100);
+        		b.setMinWidth(100);
+        		b.setMaxHeight(100);
+        		b.setMinHeight(100);
+        		gPane.add(b, i, j);
+        	}
         }
+
+        gPane.setPrefSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+        gPane.setMinSize(Region.USE_COMPUTED_SIZE, Region.USE_COMPUTED_SIZE);
 
         root.getChildren().add(gPane);
         primaryStage.setScene(scene);
